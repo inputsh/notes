b2c94394030bdaee286b3a434bbde8ccec45b9cd
aleksandar-todorovic <aleksandar.todorovic@mail.ru>
2015-02-24 23:16:59 +0100

    2014-02-24 update

--- ai/00_udacity-course/00_index.md
+++ ai/00_udacity-course/00_index.md
@@ -15,4 +15,19 @@ Notes taken from the course called Intro to Artificial Intelligence offered on U
 * **Adversarial** - the name says it all.
 
 ![image]()
-y
+
+## 2. Problem Solving
+
+Problem solving works when the domain is:
+  1. Fully observable.
+  2. Known - we know a number of possible actions.
+  3. Discrete - there is a final number of possibilities.
+  4. Deterministic - we have to know the result in advance.
+  5. Nothing can be changed during the execution (static).
+
+* **Breadth first search** - You check every single solution from the top to the bottom. You start at the first level, then you go to the second level, counting all the 2nd level steps, after which you go to the third level.
+
+* **Depth first** - You check every single solution by lowering yourself to the bottom of the tree. So, you go from level one, down to level two, down to level three and so on (marking _n_ as the lowest level possible). Then, you go to the level _n-1_ and examine if there are some other paths that you can take from there down to the _n_-th level.
+
+
+ ## Problem Set 1
---
+++ ai/01_edx-ai/tutorial/buyLotsOfFruit.py
@@ -0,0 +1,51 @@
+# buyLotsOfFruit.py
+# -----------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+To run this script, type
+
+  python buyLotsOfFruit.py
+
+Once you have correctly implemented the buyLotsOfFruit function,
+the script should produce the output:
+
+Cost of [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)] is 12.25
+"""
+
+fruitPrices = {'apples':2.00, 'oranges': 1.50, 'pears': 1.75,
+              'limes':0.75, 'strawberries':1.00}
+
+
+totalCost = 12.5
+numPounds = 0
+
+def buyLotsOfFruit(orderList):
+
+    totalCost = 12.25
+    numPonuds = 0
+    """
+        orderList: List of (fruit, numPounds) tuples
+	totalCost tuples
+
+    Returns cost of order
+    """
+
+    totalCost += numPounds
+    return totalCost
+
+# Main Method
+if __name__ == '__main__':
+    "This code runs when you invoke the script from the command line"
+    orderList = [ ('apples', 2.0), ('pears', 3.0), ('limes', 4.0) ]
+    print 'Cost of', orderList, 'is', buyLotsOfFruit(orderList)
