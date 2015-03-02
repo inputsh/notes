8b9fe72882410545e497755c87e2c4a6a87d64e2
aleksandar-todorovic <aleksandar.todorovic@mail.ru>
2015-03-02 22:11:46 +0100

    First chapter from The Singularity is Near by Ray Kurzweil

--- algorithms/code/insertionSort.py
+++ algorithms/code/insertionSort.py
@@ -18,6 +18,17 @@ def insertionSort2(alist):
     for i in range(1, len(alist)):
         bisect.insort(alist, alist.pop(i), 0, i)
 
+def reverseInsertionSort(alist):
+   for i in range(1,len(alist)):
+     tmp = alist[i]
+     pos = i
+     while pos>0 and alist[pos-1]<tmp:
+         alist[pos]=alist[pos-1]
+         pos = pos-1
+     alist[pos]=tmp
+     print(alist)
+
 alist = [5,2,4,6,1,3]
-insertionSort2(alist)
-print(alist)
+insertionSort(alist)
+print
+reverseInsertionSort(alist)
